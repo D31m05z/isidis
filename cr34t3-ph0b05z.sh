f6cc9993415703664d7bdef3254e86ce6b87e959
Syrt1s M4j0r <d31m05z@localhost.localdomain>
2018-01-30 05:02:24 -0500

    veil and mitmf framework install scripts

---
+++ cr34t3-ph0b05z.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# opencubic
+
+# conky
+git clone https://github.com/brndnmtthws/conky.git
+
+# veil framwwork
+git clone https://github.com/Veil-Framework/Veil.git
+
---
+++ v1rtu4l3nv-1n1t.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+PATH=$PATH:~/.local/bin
+source /home/d31m05z/.local/bin/virtualenvwrapper.sh
+
