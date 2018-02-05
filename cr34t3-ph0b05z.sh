8b54ecf12b9d70533416b4c3aecc0770320f52a3
Syrt1s M4j0r <d31m05z@localhost.localdomain>
2018-02-05 05:45:18 -0500

    feature: install opencubic and conky

--- cr34t3-ph0b05z.sh
+++ cr34t3-ph0b05z.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 # opencubic
+apt install --no-install-recommends opencubicplayer
 
 # conky
+apt install --no-install-recommends conky
+cp ./config/conky/conky.conf /etc/conky/conky.conf
 # git clone https://github.com/brndnmtthws/conky.git
 
 # install MITMf framework
